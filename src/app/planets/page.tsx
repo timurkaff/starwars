b0ab696c49a1e23d8ffe94af67dc713386fb2565
timurkaff <timalololohka@gmail.com>
2025-02-27 23:06:51 +0300

    fix

--- src/app/planets/page.tsx
+++ src/app/planets/page.tsx
@@ -2,6 +2,7 @@ import { getPlanets } from '@/lib/swapi';
 import Pagination from '@/components/Pagination';
 import Search from '@/components/Search';
 import Link from 'next/link';
+import { Planet } from '@/interfaces/Planet';
 
 export default async function Page({
   searchParams: searchParamsPromise,
@@ -21,7 +22,7 @@ export default async function Page({
       <h1 className="text-3xl font-bold mb-4">Планеты</h1>
       <Search placeholder="Поиск планет..." />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {results.map(planet => (
+        {results.map((planet: Planet) => (
           <div key={planet.name} className="card bg-base-200 shadow-xl">
             <div className="card-body">
               <h2 className="card-title">{planet.name}</h2>
